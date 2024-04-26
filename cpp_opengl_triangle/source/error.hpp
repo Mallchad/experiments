203b81d354dc8590194ce4b6bdd88ace95cf30db
Mallchad <ncaprisunfan@gmail.com>
2024-04-26 21:30:02 +0100

    [cpp_opengl_triangle] Removed into its own project

--- cpp_opengl_triangle/source/error.hpp
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-
-/// How'd it go? Status report on what happened to the function
-using fhowdit = bool;
-
-/// Report what happened to the user
-// In future will be an enum with associated info messages and debugging
-using freport = bool;
--- cpp_opengl_triangle/source/global.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "global.h"
-
-global_database* global_database::primary_database = nullptr;
-
-global_database* global_database::get_primary()
-{
-    return primary_database;
-}
