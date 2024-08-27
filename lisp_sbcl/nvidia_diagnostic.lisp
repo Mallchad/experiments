2804e9477e0194df5f2b54039e153748a6ac43d7
Mallchad <ncaprisunfan@gmail.com>
2024-08-27 03:16:23 +0100

    [lisp_sbcl] Bump: Working nvidia_diagnostic script
    [rust] Documentation: Tweak

---
+++ lisp_sbcl/nvidia_diagnostic.lisp
@@ -0,0 +1,8 @@
+;;(run-program "/bin/bash" :ARGUMENTS '("-c" "while [ 1 ] ; do ; nvidia-smi --query --xml-format ; sleep 0.001 ; done "))
+
+(defun main ()
+    (sb-ext:run-program "/usr/bin/nvidia-smi" '("--query" "--xml-format")
+     :output *standard-output*)
+)
+(main)
+(exit)
