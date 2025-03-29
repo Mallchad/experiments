a548925d4a699e29bffea511944a97b30e16616d
Mallchad <ncaprisunfan@gmail.com>
2025-03-29 10:19:23 +0000

    [lisp_sbcl] Added: Shebang to lisp scirpt

--- lisp_sbcl/nvidia_diagnostic.lisp
+++ lisp_sbcl/nvidia_diagnostic.lisp
@@ -1,4 +1,6 @@
-;;(run-program "/bin/bash" :ARGUMENTS '("-c" "while [ 1 ] ; do ; nvidia-smi --query --xml-format ; sleep 0.001 ; done "))
+#!/usr/bin/env -S sbcl --script
+
+;; (run-program "/bin/bash" :ARGUMENTS '("-c" "while [ 1 ] ; do ; nvidia-smi --query --xml-format ; sleep 0.001 ; done "))
 
 (defun main ()
     (sb-ext:run-program "/usr/bin/nvidia-smi" '("--query" "--xml-format")
