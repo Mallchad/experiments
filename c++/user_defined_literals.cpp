28056f667f39191394d0dd9e99f5ba0b0ad1576e
Mallchad <ncaprisunfan@gmail.com>
2025-10-16 22:22:01 +0100

    [c++] Added: string based literal examples

--- c++/user_defined_literals.cpp
+++ c++/user_defined_literals.cpp
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <bitset>
 #include "libraries/code_helpers.h"
+#include <string>
 
 using literal_integer = unsigned long long;
 using literal_float = long double;
@@ -17,6 +18,17 @@ u32 operator ""_bit(  literal_integer arg )
 u64 operator ""_bit64( literal_integer arg )
 { return (1 << arg); }
 
+u64 operator""_hex( literal_string arg, unsigned long size )
+{
+    std::string str = std::string( arg, size + 1 );
+    return std::stoll( str, nullptr, 16 );
+}
+
+u64 operator""_hex( literal_string arg )
+{
+    return std::stoll( std::string {arg}, nullptr, 16 );
+}
+
 using namespace std;
 
 int
@@ -28,4 +40,14 @@ main()
 
     reader = 31_bit;
     printf( "num1 %s\n", reader.to_string().c_str() );
+
+    u64 hex_input = "0xDEADBEEF"_hex;
+    printf( "hex_input %X \n", hex_input );
+
+    u64 hex_bare = 0xDEADBEEF_hex;
+    printf( "hex_bare %X \n", hex_bare );
+
+
 }
+
+void user_defined_literals() { main(); };
